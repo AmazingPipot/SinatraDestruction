e8d804c78c380e8228748eef01b496ddd0f6bfa6
qleroit <qleroit@p11.fdsetu.infra.umontpellier.fr>
2019-01-08 16:24:29 +0100

    debut super structure

--- build-cube-Desktop-Debug/MOTEUR_DESTRUCTION/moteur_destruction/superstructure.cpp
+++ build-cube-Desktop-Debug/MOTEUR_DESTRUCTION/moteur_destruction/superstructure.cpp
@@ -1,6 +1,13 @@
 #include "superstructure.h"
 
-SuperStructure::SuperStructure()
+SuperStructure::SuperStructure(float x, float y, float w, float h, float d, int type)
+    :indexBuf(QOpenGLBuffer::IndexBuffer),
+      X(x),
+      Y(y),
+      Width(w),
+      Height(h),
+      Depth(d),
+      typeStruct(type)
 {
 
 }
--- build-cube-Desktop-Debug/MOTEUR_DESTRUCTION/moteur_destruction/superstructure.h
+++ build-cube-Desktop-Debug/MOTEUR_DESTRUCTION/moteur_destruction/superstructure.h
@@ -1,11 +1,34 @@
 #ifndef SUPERSTRUCTURE_H
 #define SUPERSTRUCTURE_H
+//#include "destructmesh.h"
+#include <QList>
 
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
 
 class SuperStructure
 {
 public:
-    SuperStructure();
+    SuperStructure(float x, float y, float w, float h, float d, int type);
+    QList<SuperStructure> listStructure;
+    //QList<Mesh> listElementDestructible;
+
+    float X;
+    float Y;
+    float Z;
+
+    float Width;
+    float Height;
+    float Depth;
+
+    int typeStruct;
+
+    void initStruct();
+
+private:
+    QOpenGLBuffer arrayBuf;
+    QOpenGLBuffer indexBuf;
 };
 
 #endif // SUPERSTRUCTURE_H
